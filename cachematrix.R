56a800ad4f93e5e70212f9384bcd9b597bd9fa4e
bereket1219 <bereket.getachew@amu.edu.et>
2020-10-11 01:25:56 +0300

    getting ready to push an example cachematrixfile

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
+## Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## I set the input x as a matrix, the solved value "s" as a null.
+## I changed "mean" to "solve"
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix(sample(1:50,16),4,4)) {
+  s<-NULL
+  set<-function() {
+    x<<- y
+    s<<- NULL
+  }
+  get<-function() x
+  setsolve<-function(solve) s<<-solve
+  getsolve<-function() s
+  list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)
 }
 
-
-## Write a short comment describing this function
+##
+## Similarly, "mean" changed  to "solve" and "m" changed to "s"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   s<-x$getsolve()
+   if(!is.null(s)) {
+     message("getting solved data or inverse matrix")
+     return(s)
+   }
+   data<-x$get()
+   s<-solve(data,...)
+   x$setsolve(s)
+   s
 }
